3b296dcb5d12d7d92acecec52da6f60293c46518
Mahin Mirshams <m.mirshams@aut.ac.ir>
2016-07-01 12:14:48 +0430

    replacing the gifs with images

--- src/game/bufferstrategy/WalNut.java
+++ src/game/bufferstrategy/WalNut.java
@@ -11,6 +11,6 @@ class WalNut extends Plant {
     }
 
     WalNut(int x, int y, GameState state) {
-        super(x, y, "WalNut.png", 70, 70, state, 0, 300);
+        super(x, y, "wallnut.gif", 70, 70, state, 0, 300);
     }
 }
